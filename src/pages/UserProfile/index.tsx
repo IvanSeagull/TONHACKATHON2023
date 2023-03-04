3b4734a29ce93cb8e09813ad2f1fbd350024f4b2
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2023-03-04 02:35:58 +0000

    User profile page

--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Rating from '../../components/UI/Rating';
+import Links from './components/Links';
+import Reviews from './components/Reviews';
+import Skills from './components/Skills';
+
+import styles from './UserProfile.module.scss';
 
 const profile = () => {
-  return <div>profile</div>;
+  return (
+    <div className="container">
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <div className={styles.row}>
+            <h2 className={styles.name}>Jennifer Vazquez</h2>
+            <Rating />
+          </div>
+          <p className={styles.desc}>
+            UI/UX Designer, Entrepreneur I help companies build great products.
+          </p>
+        </div>
+
+        <div className={styles.msgButton}>
+          <p>Message</p>
+          <img src="/icons/msg.svg" />
+        </div>
+        <Skills />
+        <Links />
+        <Reviews />
+      </div>
+    </div>
+  );
 };
 
 export default profile;
