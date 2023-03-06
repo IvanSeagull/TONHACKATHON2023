240294b6b729b38824c402b3a20ac01b9c7b0a2b
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2023-03-06 12:22:57 +0000

    fix

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, useNavigation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigation, HashRouter } from 'react-router-dom';
 import Navbar from './components/UI/Navbar';
 import CreateJob from './pages/CreateJob';
 import Job from './pages/Job';
@@ -11,18 +11,18 @@ import UserProfile from './pages/UserProfile';
 const Navigator = () => {
   const path = 'TONHACKATHON2023';
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path={`jobs`} element={<JobLists />} />
-        <Route path={`jobs/:id`} element={<Job />} />
+        <Route path={`/jobs`} element={<JobLists />} />
+        <Route path={`/jobs/:id`} element={<Job />} />
 
-        <Route path={`profile`} element={<UserProfile />} />
-        <Route path={`create-job`} element={<CreateJob />} />
-        <Route path={`register`} element={<Registration />} />
+        <Route path={`/profile`} element={<UserProfile />} />
+        <Route path={`/create-job`} element={<CreateJob />} />
+        <Route path={`/register`} element={<Registration />} />
 
-        <Route path={``} element={<Landing />} />
+        <Route path={`/`} element={<Landing />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
